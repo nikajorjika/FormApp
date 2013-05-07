6c923738f69cdbef7a5ef52832a9012b5fc0a8a7
nikajorjika <n_jorjoliani@cu.edu.ge>
2013-05-07 18:04:37 +0400

    FormApp

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
+error_reporting(0);
 
 ?>
 
@@ -16,19 +17,16 @@ if (mysqli_connect_errno()) {
 	 
 	  <a href="index.php?student_add=new"><button>Add User</button></a>
 	  <a href="index.php?show_list=all"><button>Show List</button></a>
-	  <a href="index.php?Delete=all"><button>Delete All Students</button></a>
 	  <form action='index.php' method='get'>
-	  <input type='text' name='searchid' value='Type Students ID'>
+	  <input type='text' name='searchid' onfocus="this.value=''" value='Type Students ID'>
 	  <input type='submit' value='Edit Student'>
 	  </form>
 
 	  <?php 
 	
-	
-	$result=mysqli_query($con,'SELECT * FROM student WHERE id='.$_GET['searchid']);
-	
-	$fin=mysqli_fetch_array($result);
 
+    $result=mysqli_query($con,'SELECT * FROM student WHERE id='.$_GET['searchid']);
+	$st=mysqli_fetch_array($result);
 ?>	
 	<?php if($_GET['student_add']){ ?>
      <form action='index.php' method='post'><table>
@@ -37,26 +35,28 @@ if (mysqli_connect_errno()) {
 	 <tr><td> Gender:</td><td> <input type='radio' name='gender' value='M' />Male
 	 <input type='radio' name='gender' value='F' />Female</td></tr>
 	 <tr><td> Faculty:</td><td> <input type='text' name='faculty' /></td></tr>
-	<tr><td>Phone Number: </td><td><input type='text' name='phone' c/></td></tr>
+	<tr><td>Phone Number: </td><td><input type='text' name='phone' /></td></tr>
 	<tr><td colspan='2' align='center'><input type='submit' name='submit' value='Add Student'></td></tr>
 	 </table></form>
 	 <?php  } ?>
 	<?php if($_GET['searchid']){ ?>
+    
     <form action='index.php' method='post'><table>
-	<tr><td>ID:</td> <td><input type='text' name='editid' value=<?php echo $fin['id'];?> ></td></tr>
-   <tr><td> First Name:</td> <td><input type='text' name='editname' value=<?php echo $fin['name'];?> ></td></tr>
-	<tr> <td>Last Name:</td><td> <input type='text' name='editlastname' value=<?php echo $fin['lastname'];?>  ></td></tr>
-	<tr><td> Gender:</td><td> <input type='radio' name='editgender' value='m' checked=<?php if($fin['gender']=="M") print('checked'); ?>  >Male
-	                          <input type='radio' name='editgender' value='f'  checked=<?php if($fin['gender']=="F") print('checked'); ?> >Female</td></tr>
-	 <tr><td> Faculty:</td><td> <input type='text' name='editfaculty' value=<?php echo $fin['faculty'];?> 	></td></tr>
-	 <tr><td>Phone Number: </td><td><input type='text' name='editphone' value=<?php echo $fin['phone_num'];?>  ></td></tr>
-	 <tr><td colspan='2' align='center'><input type='submit' name='editsubmit' value='Edit Student'></td></tr>
-	</table></form>
+	  <tr><td> ID:</td> <td><input type='text' name='editid' readonly value=<?php print($st['id']);?> ></td></tr>
+     <tr><td> First Name:</td> <td><input type='text' name='name' value=<?php print($st['name']);?> ></td></tr>
+	 <tr> <td>Last Name:</td><td> <input type='text' name='lastname' value=<?php print($st['lastname']);?>  ></td></tr>
+	 <tr><td> Gender:</td><td> <input type='radio' name='gender' value='M' checked=<?php if($st['gender']=='M'){echo 'checked';}?> >Male
+	 <input type='radio' name='gender' value='F' <?php if($st['gender']=='F'){echo 'checked';}?> />Female</td></tr>
+	 <tr><td> Faculty:</td><td> <input type='text' name='faculty'value=<?php print($st['faculty']);?> ></td></tr>
+	 <tr><td>Phone Number: </td><td><input type='text' name='phone' value=<?php print($st['phone_num']);?>></td></tr>
+	 <tr><td colspan='2' align='center'><input type='submit' name='editsubmit' value='Update Info'></td></tr>
+	 </table></form>	
 	<?php }
-	print_r($_POST['editgender']);
+	if($_POST['editsubmit']){
+	
 	mysqli_query($con, "UPDATE student SET name='".$_POST['name']."', lastname='".$_POST['lastname']."', gender='".$_POST['gender']."', faculty='".$_POST['faculty']."', phone_num='".$_POST['phone']."'  WHERE id='".$_POST['editid']."'");
-	$update="UPDATE student SET name='".$_POST['name']."', lastname='".$_POST['lastname']."', gender='".$_POST['gender']."', faculty='".$_POST['faculty']."', phone_num='".$_POST['phone']."'  WHERE id='".$_POST['editid']."'";
-
+	
+     }
 	?>
 	
 	 
@@ -66,18 +66,6 @@ if (mysqli_connect_errno()) {
 
 
 	  
-<?php
-if($_GET['delete']='all')
-{
-mysqli_query($con,'DELETE FROM users WHERE id=* ');
-
-
-}
-
-
-
-
-?>	  
 	 
 	  
 <?php 
